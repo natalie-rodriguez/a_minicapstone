e18c79196d74e5a32178e2d96dfae16c875baa36
natalie <nataliediazrodriguez@gmail.com>
2019-07-31 16:32:59 -0400

    add products to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ product = Product.new(name: "jade pathos", price: 28, description: "It comes potted in our soil mix to increase plant health, longevity, and growthy", image_url: "http://www.asupportingrole.com/i/2015/08/exciting-interior-potted-plant-with-pothos-plant-for-traditional-kitchen-design-pothos-pothos-plant-in-water-pothos-plant-care-pathos-plant-pothos-plants-golden-pothos-plant-yellow-leaves-pothos-plant.jpg")
+ product.save
+
+ product = Product.new(name: "Bird nest fern", price: 22, description: "The Bird’s Nest Fern is paired with our Dolores Planter, an earthenware pot measuring 7 tall and 7 wide ", image_url: "https://cdn.shopify.com/s/files/1/0013/3529/6118/products/Reverse-Taper-4P-4-Birdsnest-Nidus.jpg?v=1557436610")
+
+ product.save
+ 
+ product = Product.new(name: "Calathea Pinstripe", price: 62, description: "Your plant height may vary slightly", image_url: "https://smartgardenguide.com/wp-content/uploads/2019/01/how-to-care-for-calathea-ornata-8.jpg")
+
+ product.save
